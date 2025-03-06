ca71314e14c42b44fcf871945add324ed3c931cb
moududulhaque <moududulhaque@gmail.com>
2025-03-06 23:09:36 +0600

    Map Added on contact page

--- contact.php
+++ contact.php
@@ -4,4 +4,8 @@
     <p>Get in touch with us through our contact page. We are here to help you with any questions or concerns. Whether you have inquiries about our services, need support with a project, or just want to provide feedback, we are always available to assist you. Our team is dedicated to providing prompt and helpful responses to ensure your satisfaction. You can reach us via email, phone, or by filling out the contact form on our website. We value your input and look forward to hearing from you. Thank you for choosing our services.</p>
 </section>
 
+<div class="map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d115947.87529040614!2d90.31391744153466!3d24.748462222140766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37564f1007ad9b59%3A0x79a59cb060e32d6c!2sMymensingh!5e0!3m2!1sen!2sbd!4v1741280882755!5m2!1sen!2sbd" width="400" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+
 <?php include_once('footer.php'); ?>
