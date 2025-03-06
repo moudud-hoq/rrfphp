c4354d2056b7228783f82ac8751a2c71bab5cb78
moududulhaque <moududulhaque@gmail.com>
2025-03-07 00:01:16 +0600

    Active menu will be another color

--- header.php
+++ header.php
@@ -9,16 +9,37 @@
 </head>
 
 <body>
+
+    <!-- <?php echo var_dump(value: $_SERVER['REQUEST_URI']); ?>
+    <?php
+    $uri = $_SERVER['REQUEST_URI'];
+    $path = parse_url($uri, PHP_URL_PATH);
+    echo $path;
+    ?> -->
+
+    <?php
+    function is_active($check)
+    {
+        $requested_uri = $_SERVER['REQUEST_URI'];
+
+        $search = strpos($requested_uri, $check);
+        if ($search !== false) {
+            echo 'active';
+        }
+    }
+
+    ?>
+
     <header>
         <h1>Welcome to My Website</h1>
         <p>This is a simple website structure with some CSS and HTML.</p>
     </header>
     <nav>
-        <a href="index.php">Home</a>
-        <a href="about.php">About</a>
-        <a href="contact">Contact</a>
-        <a href="services">Services</a>
-        <a href="portfolio">Portfolio</a>
+        <a class="<?php echo is_active('index'); ?>" href="index.php">Home</a>
+        <a class="<?php echo is_active('about'); ?>"  href="about.php">About</a>
+        <a class="<?php echo is_active('contact'); ?>"  href="contact">Contact</a>
+        <a class="<?php echo is_active('services'); ?>"  href="services">Services</a>
+        <a class="<?php echo is_active('portfolio'); ?>"  href="portfolio">Portfolio</a>
     </nav>
 
     <div class="content">
