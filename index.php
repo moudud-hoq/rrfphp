d05f21ba09481842910a2fea58914a541568b62a
moududulhaque <moududulhaque@gmail.com>
2025-03-06 18:51:55 +0600

    Change the file

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=h1, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Welcome to My Website</h1>
+    <p>This is a simple website structure with some CSS and HTML.</p>
+    <style>
+
+    </style>
+    <header>
+        <h2>My Website Header</h2>
+    </header>
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+    </nav>
+    <footer>
+        <p>&copy; Moududul Hoque <?php
+                                    date_default_timezone_set('Asia/Dhaka'); ?> - <?php echo date('l jS \of F Y h:i:s A');
+                                                                                    ?></p>
+    </footer>
+</body>
+
+</html>
